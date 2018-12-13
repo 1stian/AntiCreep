770e893d5852295be3fa95b6bdf8ed9f9e0df5c2
Stian Tofte <stitofte@outlook.com>
2018-12-13 22:29:09 +0100

    Removed unused import.

--- AntiCreep/src/com/onestian/anticreep/creeperListener.java
+++ AntiCreep/src/com/onestian/anticreep/creeperListener.java
@@ -1,7 +1,6 @@
 package com.onestian.anticreep;
 
 import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityExplodeEvent;
