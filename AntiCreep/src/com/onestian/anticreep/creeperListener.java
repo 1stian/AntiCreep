86d4b6a868ae1db29e29f6308ac7227699e6bb95
Stian Tofte <Stian Tofte@StianPC>
2018-12-16 01:18:20 +0100

    Added custom explosion. Testing..

--- AntiCreep/src/com/onestian/anticreep/creeperListener.java
+++ AntiCreep/src/com/onestian/anticreep/creeperListener.java
@@ -1,5 +1,6 @@
 package com.onestian.anticreep;
 
+import org.bukkit.Location;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -7,16 +8,39 @@ import org.bukkit.event.entity.EntityExplodeEvent;
 
 public class creeperListener implements Listener {
 	
+	boolean blockDmg = readConfig.getblock();
+	boolean customRadius = readConfig.getCustom();
+	int radius = readConfig.getRadius();
+	
 	@EventHandler
 	public void creeperExplode(EntityExplodeEvent event) {
-		
+		String bDmg = Boolean.toString(blockDmg);
 		EntityType enty = event.getEntityType();
 		
-		if (readConfig.getblock()) {
+		if (blockDmg) {
 			if (enty == EntityType.CREEPER) {
 				event.setCancelled(true);
 			}
 		}
+		
+		if (customRadius) {
+			if (enty == EntityType.CREEPER) {
+				//Getting creeper location
+				Location creeperLoc = event.getLocation();
+				
+				//Canceling creeper explosion
+				event.setCancelled(true);
+				
+				//Creating custom explosion
+				switch (bDmg.toLowerCase()) {
+				case "true":
+					creeperLoc.getWorld().createExplosion(creeperLoc, radius, false);
+					break;
+				case "false":
+					creeperLoc.getWorld().createExplosion(creeperLoc.getX(), creeperLoc.getY(), creeperLoc.getZ(), radius, false, false);
+					break;
+				}
+			}
+		}
 	}
-
 }
